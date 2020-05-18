8a84af2ce765ed9637825f16d17c3b220626e675
rbflurry <rbflurry@gmail.com>
2020-05-18 09:32:20 -0400

    updates

--- bringupcom0.sh
+++ bringupcom0.sh
@@ -2,7 +2,7 @@
   while true
     do 
       socat pty,link=/dev/ttyUSB0,raw,user=0,group=0,mode=777 tcp:192.168.47.168:3333
-      sleep 1
+      sleep 60
     done
 
 exit
--- bringupcom1.sh
+++ bringupcom1.sh
@@ -2,6 +2,6 @@
   while true
     do 
       socat pty,link=/dev/ttyUSB1,raw,user=0,group=0,mode=777 tcp:192.168.47.168:3334
-      sleep 1
+      sleep 60
     done
 
