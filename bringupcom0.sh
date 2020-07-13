0cf25f87ecef9956277adbec51fb6eadb05a6742
rbflurry <rbflurry@gmail.com>
2020-07-13 13:15:54 -0400

    long time coming

--- bringupcom0.sh
+++ bringupcom0.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
   while true
     do 
-      socat pty,link=/dev/ttyUSB0,raw,user=0,group=0,mode=777 tcp:192.168.47.168:3333
+      socat pty,link=/dev/ttyUSB0,raw,user=0,group=0,mode=777 tcp:192.168.47.206:3333
       sleep 60
     done
 
