dd7508c0aa8c0d04b965de74c6834a8e6e59f3a0
rbflurry <rbflurry@gmail.com>
2020-08-12 17:26:50 -0400

    making some changes
    smoke alarms wled bed.

--- bringupcom0.sh
+++ bringupcom0.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#zwave
   while true
     do 
       socat pty,link=/dev/ttyUSB0,raw,user=0,group=0,mode=777 tcp:192.168.47.206:3333
--- bringupcom1.sh
+++ bringupcom1.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#zigbee
+exit
   while true
     do 
       socat pty,link=/dev/ttyUSB1,raw,user=0,group=0,mode=777 tcp:192.168.47.206:3334
