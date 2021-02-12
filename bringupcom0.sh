cefc625c0e260a9c1acdd54aefa446142e130b60
rbflurry <rbflurry@gmail.com>
2021-02-11 23:51:28 -0500

    switch to zwavejsmqtt

--- bringupcom0.sh
+++ bringupcom0.sh
@@ -2,7 +2,7 @@
 #zwave
   while true
     do 
-      socat pty,link=/dev/ttyUSB0,raw,user=0,group=0,mode=777 tcp:192.168.47.206:3333
+      #socat pty,link=/tmp/ttyUSB0,raw,user=0,group=0,mode=777 tcp:192.168.47.206:3333
       sleep 60
     done
 
