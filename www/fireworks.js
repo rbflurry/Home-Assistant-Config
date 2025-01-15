ec4e8c86092edee9e6dd0326b0ec26424c4affb4
rbflurry <rbflurry@gmail.com>
2025-01-15 18:06:13 -0500

    before 2025

---
+++ www/fireworks.js
@@ -0,0 +1,5 @@
+import { Fireworks } from 'fireworks-js'
+
+const container = document.querySelector('.container')
+const fireworks = new Fireworks(container, { /* options */ })
+fireworks.start()
--- www/snowflakes.js
+++ www/snowflakes.js
@@ -1,2 +1,17 @@
 import Snowflakes from 'https://cdn.skypack.dev/magic-snowflakes';
-new Snowflakes();
+
+// always run
+ new Snowflakes();
+
+// OR use date
+// month in JS dates are zero-based
+const currentMonth = new Date().getMonth() + 1;
+const currentDay = new Date().getDate();
+
+// run from december 6th to january 15th 
+const isOkForDecember = currentMonth === 12 && currentDay >= 6;
+const isOkForJanuary = currentMonth === 1 && currentDay <= 15;
+if (isOkForDecember || isOkForJanuary ) {
+ // new Snowflakes();  
+}
+
